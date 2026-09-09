7ae689a0aa32ed3f30bc43d06ad94aca30c28838
Nine1Eight <founder918tech@gmail.com>
2026-09-09 11:32:46 -0500

    Test installed GlyphMatics public API

--- tests/integration/test_full_chain.py
+++ tests/integration/test_full_chain.py
@@ -1,7 +1,8 @@
-from src.glyphmatics.component import GlyphMaticsEngine
+from glyphmatics.component import GlyphMaticsEngine
+
 
 def test_full_chain():
     engine = GlyphMaticsEngine()
     state = engine.run_universe()
     assert state["U_score"] > 0.999
-    assert state["arc_demo_ok"] is True  # from internal demo
+    assert state["arc_demo_ok"] is True
